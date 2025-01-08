47928b570d48f607c7bbb057cf97552e47d89be0
francesco-bernardi <francesco.bernardi2010@live.it>
2025-01-08 18:34:27 +0100

    update gitignore

--- fair_spoke_8_leonardo/scripts/download_laion_hd_subset.py
+++ fair_spoke_8_leonardo/scripts/download_laion_hd_subset.py
@@ -2,19 +2,23 @@ from datasets import load_dataset
 from tqdm import tqdm
 from pathlib import Path
 
-def save_urls_to_txt(dataset_split, filename):
+def save_field_to_txt(dataset_split, filename, field: str):
     with open(filename, 'w', encoding='utf-8') as f:
         size = len(dataset_split)
         for entry in tqdm(dataset_split, total=size):
-            f.write(entry['url'] + '\n')
+            f.write(entry[field] + '\n')
 
 def main():
     ds = load_dataset("yuvalkirstain/laion-hd-subset", split=None)
 
     if not Path('train_urls.txt').exists():
-        save_urls_to_txt(ds['train'], 'train_urls.txt')
+        save_field_to_txt(ds['train'], 'train_urls.txt', 'url')
+    if not Path('train_cap.txt').exists():
+        save_field_to_txt(ds['train'], 'train_cap.txt', 'caption')
     if not Path('test_urls.txt').exists():
-        save_urls_to_txt(ds['test'], 'test_urls.txt')
+        save_field_to_txt(ds['test'], 'test_urls.txt', 'url')
+    if not Path('test_cap.txt').exists():
+        save_field_to_txt(ds['test'], 'test_cap.txt', 'caption')
 
     print(ds["train"][0])
     
