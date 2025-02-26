57d7e89a6ca8b717dc70936a79cba64b04ea009b
Leonardo Baroncelli <leonardo.baroncelli26@gmail.com>
2025-02-26 10:52:19 +0100

    Set correct parallelization of tasks

--- data_quality_pipeline/src/made/data_pipeline/pipeline.py
+++ data_quality_pipeline/src/made/data_pipeline/pipeline.py
@@ -3,6 +3,9 @@ from pathlib import Path
 import ray
 
 from made.data_pipeline.steps.unimodal_text_filtering import ray_unimodal_text_filtering
+from made.data_pipeline.steps.unimodal_vision_filtering import ray_unimodal_vision_filtering
+#from made.data_pipeline.steps.multimodal_filtering import ray_multimodal_filtering
+#from made.data_pipeline.steps.vision_deduplication import ray_vision_deduplication
 from made.config import Config
 
 ray.init()
@@ -13,24 +16,40 @@ def split_and_distribute_dataset(tar_files: list[str | Path], num_workers: int):
     # Ensure each worker gets a subset of tar files
     shard_splits = [tar_files[i::num_workers] for i in range(num_workers)]
     
+
     # Launch Ray tasks
-    unimodal_text_filtering_results = ray.get(
-        [ray_unimodal_text_filtering.remote(shards) for shards in shard_splits if shards]
-    )
+    text_filtering_futures = [
+        ray_unimodal_text_filtering.remote(shards) for shards in shard_splits if shards
+    ]
+    vision_filtering_futures = [
+        ray_unimodal_vision_filtering.remote(shards) for shards in shard_splits if shards
+    ]
 
-    # unimodal_vision_filtering_results = ray.get(
-    #     [ray_unimodal_vision_filtering.remote(shards) for shards in shard_splits if shards]
-    # )
+    unimodal_text_filtering_results = ray.get(text_filtering_futures)
+    unimodal_vision_filtering_results = ray.get(vision_filtering_futures)
 
-    # multimodal_filtering_results = ray.get(
-    #     [ray_multimodal_filtering.remote(shards, unimodal_text_filtering_results, unimodal_vision_filtering_results) for shards in shard_splits if shards]
-    # )
+    # Concatenate results
+    concatenated_text_results = sum(unimodal_text_filtering_results, [])
+    concatenated_vision_results = sum(unimodal_vision_filtering_results, [])
 
-    # vision_deduplication_results = ray.get(
-    #     [ray_vision_deduplication.remote(shards, multimodal_filtering_results) for shards in shard_splits if shards]
-    # )
+    """
+    # Pass results to multimodal filtering along with the original input
+    multimodal_filtering_futures = [
+        ray_multimodal_filtering.remote(shards, concatenated_text_results, concatenated_vision_results) 
+        for shards in shard_splits if shards
+    ]
 
+    multimodal_filtering_results = ray.get(multimodal_filtering_futures)
 
+    vision_deduplication_futures = [
+        ray_vision_deduplication.remote(shards, multimodal_filtering_results)
+        for shards in shard_splits if shards
+    ]
+
+    vision_deduplication_results = ray.get(vision_deduplication_futures)
+    """
+
+    return None
 
 if __name__ == "__main__":
 
