c7440a40e7ed9dad78a4a9b4d331d6823ee8d913
francesco-bernardi <francesco.bernardi2010@live.it>
2025-03-18 11:04:28 +0100

    modified unimodal test with tagging model

--- data_quality_pipeline/test/test_unimodal_text_filtering.py
+++ data_quality_pipeline/test/test_unimodal_text_filtering.py
@@ -2,6 +2,7 @@ import pytest
 import logging  
 import ray
 import fasttext
+import spacy
 from made.paths import MADE_PATH
 
 from made.data_pipeline.steps.unimodal_text_filtering import (
@@ -12,9 +13,15 @@ from made.config import Config
 from made.data_pipeline.metrics.metrics_store import MetricsStore
 
 def test_unimodal_text_filtering(tar_files, log_folder, config):
-    language_detection_model = fasttext.load_model(str(MADE_PATH / config.unimodal.lang_detection_model_path))
+    language_detection_model = fasttext.load_model(
+        str(MADE_PATH / config.unimodal.lang_detection_model_path)
+        )
+    spacy.require_gpu()
+    tagging_model = spacy.load(
+        str(config.unimodal.tagging_model_path)
+        )
 
-    results = unimodal_text_filtering(language_detection_model, tar_files, log_folder, config)
+    results = unimodal_text_filtering(language_detection_model,tagging_model, tar_files, log_folder, config)
 
 def test_ray_unimodal_text_filtering(ray_init, ray_flag, tar_files, log_folder, config_path):
     if not ray_flag:
