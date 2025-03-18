25757da24cd6185eec0c50153b5bf42e7dbfad9c
francesco-bernardi <francesco.bernardi2010@live.it>
2025-03-18 11:02:37 +0100

    added filtering by caption lenght and PoS tagging

--- data_quality_pipeline/src/made/data_pipeline/steps/unimodal_text_filtering.py
+++ data_quality_pipeline/src/made/data_pipeline/steps/unimodal_text_filtering.py
@@ -2,6 +2,7 @@ from pathlib import Path
 import ray
 import logging
 import fasttext
+import spacy
 from itertools import chain
 from datetime import datetime
 
@@ -13,16 +14,37 @@ from made.data_pipeline.data.datacomp_handler import decode_webdataset, get_next
 
 @ray.remote
 class UnimodalTextFilter:
+    with open('/home/fbernardi/Documents/fair_spoke_8/data_quality_pipeline/test/data/common_pos_patterns.txt', 'r') as file:
+        common_pos_patterns = [line.strip() for line in file.readlines()]
+
     def __init__(self, config_path: Path):
         self.config = Config(config_path)
-        self.language_detection_model = fasttext.load_model(str(MADE_PATH / self.config.unimodal.lang_detection_model_path))
+        self.language_detection_model = fasttext.load_model(
+            str(MADE_PATH / self.config.unimodal.lang_detection_model_path)
+            )
+        self.tagging_model = spacy.load(
+            str(MADE_PATH / self.config.unimodal.tagging_model_path)
+        )
     
     def ray_unimodal_text_filtering(self, tar_files: list[str | Path], log_folder: Path):
         _ = MetricsStore()
-        return unimodal_text_filtering(self.language_detection_model, tar_files, log_folder, self.config)
+        return unimodal_text_filtering(
+            self.language_detection_model,
+            self.tagging_model,
+            tar_files, 
+            log_folder, 
+            self.config
+        )
 
 
-def unimodal_text_filtering(language_detection_model, tar_files: list[str | Path], log_folder: Path, config: Config):
+def unimodal_text_filtering(
+        language_detection_model,
+        pos_tagging_model,
+        tar_files: list[str | Path], 
+        log_folder: Path, 
+        config: Config
+    ):
+    
     logger = logging.getLogger("ray")
 
     # logger.info("Validating configuration")
@@ -54,8 +76,16 @@ def unimodal_text_filtering(language_detection_model, tar_files: list[str | Path
 
         # ------------------------------------------- 
         # first step: filter by caption length
-        # TODO: implement this
-
+        ok_uids, ok_samples, uids_filtered, samples_filtered = apply_filtering_step(
+            filter_name=_get_filter_captions_by_length_mask,
+            batch_id=batch_id,
+            uids=batch[0],
+            samples=batch[1],
+            parameters = {
+                "min_words": config.unimodal.caption_min_words,
+                "min_chars": config.unimodal.caption_max_chars
+            }
+        )
 
         # ------------------------------------------- 
         # second step: filter by language
@@ -74,8 +104,16 @@ def unimodal_text_filtering(language_detection_model, tar_files: list[str | Path
 
         # ------------------------------------------- 
         # third step: pos tags filtering
-        # TODO: implement this
-
+        ok_uids, ok_samples, uids_filtered, samples_filtered = apply_filtering_step(
+            filter_name = _get_filter_captions_by_pos_tags_mask,
+            batch_id=batch_id,
+            uids=batch[0],
+            samples=batch[1],
+            parameters = {
+                "model": pos_tagging_model,
+                "target_pos_tags": UnimodalTextFilter.common_pos_patterns
+            }
+        )
 
         # ------------------------------------------- 
         # fourth step: text specificity filtering
@@ -92,6 +130,30 @@ def unimodal_text_filtering(language_detection_model, tar_files: list[str | Path
         MetricsStore().save_to_file(log_folder)
     return all_uids
 
+# TODO: implement a function than clean the captions and 
+# remove extra whitespace and newlines
+
+def _get_filter_captions_by_length_mask(
+        captions: list[str],
+        min_words: int,
+        min_chars: int
+    ) -> list[bool]:
+    
+    cleaned_captions = [
+        caption.strip().replace('\n', ' ') for caption in captions
+        ]
+    
+    tot_words = [len(caption.split()) for caption in cleaned_captions]
+    tot_chars = [len(caption) for caption in cleaned_captions]
+
+    # Create mask checking both conditions for each caption
+    mask = [
+        tot_words >= min_words and tot_chars >= min_chars
+        for tot_words, tot_chars in zip(tot_words, tot_chars)
+    ]
+
+    return mask
+        
 def _get_filter_captions_by_language_mask(
         captions: list[str],
         model: fasttext.FastText,
@@ -104,10 +166,28 @@ def _get_filter_captions_by_language_mask(
     predictions, scores = model.predict(captions)
 
     return [
-        (pred == f"__label__{target_language}") and (score > threshold)
+        (pred[0] == f"__label__{target_language}") and (score[0] > threshold)
         for pred, score in zip(predictions, scores)
     ]
+
+def _get_filter_captions_by_pos_tags_mask(
+        captions: list[str],
+        model: spacy.Language,
+        target_pos_tags: list[str]
+    ) -> list[bool]:
+
+    mask = []
+
+    for caption in captions:
+        doc = model(caption)
+        pos_tags = [token.pos_ for token in doc]
+        sorted_unique_tags = sorted(set(pos_tags))
+        pos_string = "_".join(sorted_unique_tags)
+        is_good_pos = pos_string in target_pos_tags
+        mask.append(is_good_pos)
     
+    return mask
+
 
 def _validate_configuration(config: Config):
     if config.unimodal.lang_detection_language not in ["en", "it", "es"]:
