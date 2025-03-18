faac99546e9000635a18f87a250eebda8e277c0b
francesco-bernardi <francesco.bernardi2010@live.it>
2025-03-18 11:05:47 +0100

    modified config test file

--- data_quality_pipeline/test/conftest.py
+++ data_quality_pipeline/test/conftest.py
@@ -69,23 +69,36 @@ def config_path(request):
     with open(config_file_for_tests_path, "w") as f:
         f.write(f"""
 infrastructure:
-    num_workers: 2 
-    enable_metrics: true
-    logging_level: DEBUG
-    save_npy: true
+  num_workers: 8 
+  enable_metrics: true
+  logging_level: DEBUG
+  save_npy: true
 
 unimodal:
-    batch_size: 20
+  batch_size: 4000
 
-    caption_min_length: 10
-    caption_max_length: 100
+  caption_min_words: 2
+  caption_max_length: 100
+  caption_max_chars: 5
 
-    lang_detection_model_path: models/lid.176.bin
-    lang_detection_score_threshold: 0.7
-    lang_detection_language: en
+  lang_detection_model_path: models/lid.176.bin
+  lang_detection_score_threshold: 0.7
+  lang_detection_language: en
 
-    image_min_aspect_ratio: 0.8
-    image_max_aspect_ratio: 1.8
+  tagging_model_path: en_core_web_trf
+  good_captions_pos_distribution_path: models/blabla.pickle
+
+  image_min_aspect_ratio: 0.8
+  image_max_aspect_ratio: 1.8
+
+multimodal:
+  batch_size: 32
+  clip_processor: "openai/clip-vit-base-patch32"
+  clip_model: "openai/clip-vit-base-patch32"
+  caption_generation_processor: "llava-hf/llava-v1.6-mistral-7b-hf"
+  caption_generator_model: "llava-hf/llava-v1.6-mistral-7b-hf"
+  diffusion_model: ""
+  clip_score_threshold: 0.5
 
 """)
     return config_file_for_tests_path
